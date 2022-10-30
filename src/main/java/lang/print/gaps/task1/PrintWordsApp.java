d6d742efdf962eca1a2e57befff0765358816be3
wilkat <katarzyna.f.wilk@gmail.com>
2022-10-30 19:25:34 +0100

    New printing commands.

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("\"I'm updated \n can be compiled. And can write to console\"");
+        System.out.println("java\nis\nsmth");
     }
 }
