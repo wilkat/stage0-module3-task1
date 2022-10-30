a45022020766141deaf9ac59882e938204358c5e
wilkat <katarzyna.f.wilk@gmail.com>
2022-10-30 19:10:40 +0100

    New printing commands.

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
+        System.out.println("\"I\'m updated \n can be compiled. And can write to console\"");
     }
 }
